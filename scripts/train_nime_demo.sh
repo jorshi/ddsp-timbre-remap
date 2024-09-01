b8f84e7e7a4c29f88d80f383bee9be71a8e9a6e2
jorshi <j.m.shier@qmul.ac.uk>
2024-09-02 00:05:39 +0200

    presets and script for training mapping models

---
+++ scripts/train_nime_demo.sh
@@ -0,0 +1,24 @@
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_snare_1.json --data.audio_path audio/nime-demo/water_bottle.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_snare_1.json --data.audio_path audio/nime-demo/snare_acoustic_multi_rod.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_snare_1.json --data.audio_path audio/nime-demo/snare_bright_stick.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_snare_1.json --data.audio_path audio/nime-demo/break.wav --trainer.default_root_dir nime_demo/
+
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_snare_3.json --data.audio_path audio/nime-demo/water_bottle.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_snare_3.json --data.audio_path audio/nime-demo/snare_acoustic_multi_rod.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_snare_3.json --data.audio_path audio/nime-demo/snare_bright_stick.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_snare_3.json --data.audio_path audio/nime-demo/break.wav --trainer.default_root_dir nime_demo/
+
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_kick.json --data.audio_path audio/nime-demo/water_bottle.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_kick.json --data.audio_path audio/nime-demo/kick_dry_felt.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_kick.json --data.audio_path audio/nime-demo/kick_bright_plastic.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_kick.json --data.audio_path audio/nime-demo/break.wav --trainer.default_root_dir nime_demo/
+
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_tom_1.json --data.audio_path audio/nime-demo/water_bottle.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_tom_1.json --data.audio_path audio/nime-demo/tom_sixteen_inch.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_tom_1.json --data.audio_path audio/nime-demo/high_tom_studio.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_tom_1.json --data.audio_path audio/nime-demo/break.wav --trainer.default_root_dir nime_demo/
+
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_tom_2.json --data.audio_path audio/nime-demo/water_bottle.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_tom_2.json --data.audio_path audio/nime-demo/tom_sixteen_inch.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_tom_2.json --data.audio_path audio/nime-demo/high_tom_studio.wav --trainer.default_root_dir nime_demo/
+timbreremap fit -c cfg/nime_demo.yaml --model.preset cfg/presets/808_tom_2.json --data.audio_path audio/nime-demo/break.wav --trainer.default_root_dir nime_demo/
