53edb744d10f42df330b6af55749d8d3e97fa33b
jorshi <j.m.shier@qmul.ac.uk>
2024-11-12 11:28:31 +0000

    adding a feature factory

---
+++ timbreremap/feature_factory.py
@@ -0,0 +1,20 @@
+from .feature import FeatureCollection
+from .feature import SpectralCentroid
+from .feature import SpectralFlatness
+from .feature import Loudness
+from .feature import CascadingFrameExtactor
+
+def simple_spectral_factory(sample_rate: int):
+    return FeatureCollection([SpectralCentroid(sample_rate=sample_rate), SpectralFlatness()])
+
+def spectral_loudness_factory(sample_rate: int, num_samples: int):
+    centroid = SpectralCentroid(sample_rate=sample_rate)
+    loudness = Loudness(sample_rate=sample_rate)
+
+    frame_size = 2048
+    hop_size = 512
+    num_frames = num_samples // hop_size
+    print(num_frames)
+
+    extractor = CascadingFrameExtactor([centroid, loudness], num_frames=[num_frames,], frame_size=frame_size, hop_size=hop_size, include_diff=True)
+    return extractor
